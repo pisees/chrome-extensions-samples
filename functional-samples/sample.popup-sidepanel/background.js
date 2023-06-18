bda88e49797f24937bfc9f1da010d342cd685dc0
pisees <kschenk@hotmail.com>
2023-06-18 16:30:27 -0400

    Now yet working need background.service_worker key

--- functional-samples/sample.popup-sidepanel/background.js
+++ functional-samples/sample.popup-sidepanel/background.js
@@ -1,3 +1,4 @@
-chrome.browserAction.onClicked.addListener(function(tab){
+chrome.action.onClicked.addListener(tab => {
     chrome.tabs.sendMessage(tab.id,"toggle");
-});
+    console.log('message sent');
+  });
--- functional-samples/sample.popup-sidepanel/content.js
+++ functional-samples/sample.popup-sidepanel/content.js
@@ -1,8 +1,11 @@
+console.log("side-panel script loaded");
+
 chrome.runtime.onMessage.addListener(function(msg, sender){
     if(msg == "toggle"){
+        console.log("message received");
         toggle();
     }
-});
+})
 
 var iframe = document.createElement('iframe'); 
 iframe.style.background = "green";
@@ -12,8 +15,8 @@ iframe.style.position = "fixed";
 iframe.style.top = "0px";
 iframe.style.right = "0px";
 iframe.style.zIndex = "9000000000000000000";
-iframe.frameBorder = "none"; 
-iframe.src = chrome.extension.getURL("popup.html")
+iframe.style.border = "0px"; 
+iframe.src = chrome.runtime.getURL("popup.html")
 
 document.body.appendChild(iframe);
 

fc4e82d36f7a32bef8c18b56c6ddb582745d1be2
pisees <kschenk@hotmail.com>
2023-06-20 10:54:02 -0400

    remove popu-sidePanel, I created an extension instead.

--- functional-samples/sample.popup-extension2/content.js
+++ functional-samples/sample.popup-extension2/content.js
@@ -18,8 +18,8 @@ iframe.style.top = "0px";
 iframe.style.right = "0px";
 iframe.style.zIndex = "9000000000000000000";
 iframe.style.border = "0px"; 
-iframe.src = chrome.runtime.getURL("popup.html")
-//iframe.src = "https://www.bing.com/chat"
+//iframe.src = chrome.runtime.getURL("popup.html")
+iframe.src = "https://www.bing.com/chat"
 //param = "?key=dmywSbpKA6Xk2eqL7Qtk9Hs"
 //iframe.src = "https://httbin.org/get?{param}"
 //iframe.src = "https://jsonviewer.stack.hu/#http://httpbin.org/get?key=1234567"
@@ -28,7 +28,7 @@ document.body.appendChild(iframe);
 
 const accessToken = getAccessToken();
 //TODO: Access to Tab DOM content
-alert (accessToken)
+//alert (accessToken)
 
 function toggle(){
     if(iframe.style.width == "0px"){
@@ -38,24 +38,3 @@ function toggle(){
         iframe.style.width="0px";
     }
 }
-
-function getAccessToken() {
-
-    const resp = fetch("https://chat.openai.com/api/auth/session",
-        {
-            method: 'GET',
-            credentials: 'include'
-        }
-    )
-    credentials: 'include'
-    if (resp.ok) { // if HTTP-status is 200-299
-        // get the response body (the method explained below)
-        let json = resp.json();
-      } else {
-        alert("HTTP-Error: " + resp.status);
-      }
-    if (!resp.accessToken) {
-      throw new Error("UNAUTHORIZED");
-    }
-    return resp.accessToken;
-  }
